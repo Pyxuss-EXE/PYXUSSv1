270a0851298ffcfe0543e34d5b06fef1b317c99c
Official_Pyxuss <parbwjhusen@gmail.com>
2024-07-01 23:01:29 +0545

    Update run.py

--- run.py
+++ run.py
@@ -13,7 +13,7 @@ os.system('git pull --quiet 2>/dev/null')
 ranaxd = platform.architecture()[0]
 if ranaxd == '64bit':
     print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mYour Device is 64bit')
-    import run2.cpython_311 as run2
+    import run2.cpython_311
 elif ranaxd == '32bit':
     print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mYour Device is 32bit')
-    import run2.cpython_311 as run2
+    import run2.cpython_311 
