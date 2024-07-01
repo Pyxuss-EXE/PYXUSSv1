ae4cf565cb235099d138bd798e755f4eee6f1253
Official_Pyxuss <parbwjhusen@gmail.com>
2024-07-01 22:58:53 +0545

    Add files via upload

--- run.py
+++ run.py
@@ -1,9 +1,19 @@
-import os,platform
-os.system('git pull')
-os.system("clear")
-print('\033[92;1m WELCOME TO CLONING TOOL')
-fbd=platform.architecture()[0]
-if fbd=="32bit":
-    __import__("run2")
-elif fbd=="64bit":
-    __import__("run2")
+import os
+import platform
+import time
+import sys
+os.system('pkg install espeak -y')
+try:
+    import requests
+except:
+    os.system("pip uninstall requests -y; pip install requests")
+
+print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mChecking For Update...')
+os.system('git pull --quiet 2>/dev/null')
+ranaxd = platform.architecture()[0]
+if ranaxd == '64bit':
+    print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mYour Device is 64bit')
+    import run2.cpython_311 as run2
+elif ranaxd == '32bit':
+    print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mYour Device is 32bit')
+    import run2.cpython_311 as run2
